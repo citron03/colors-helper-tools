ee55fab127b1d178470c97a162b9c50926d9ea12
citron03 <cchan0617@gmail.com>
2024-06-07 22:12:54 +0900

    setting terser & package.json bin

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import terser from '@rollup/plugin-terser';
 
 export default defineConfig({
   input: 'index.ts',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-    minifyInternalExports: true,
-  },
-  plugins: [typescript()],
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      minifyInternalExports: true,
+    },
+    {
+      file: 'dist/index.min.js',
+      format: 'iife',
+      name: 'version',
+      plugins: [terser()],
+    },
+  ],
+  plugins: [typescript(), json()],
 });
