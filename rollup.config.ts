1f06bfb7c2ad64f49cd634f309d481a7c49a1b7a
citron03 <cchan0617@gmail.com>
2024-08-17 23:36:34 +0900

    fix: rollup settings sourcemap

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,15 +10,15 @@ export default defineConfig({
     {
       dir: 'dist',
       format: 'cjs',
-      // minifyInternalExports: true,
-      // sourcemap: true,
+      minifyInternalExports: true,
+      sourcemap: true,
       entryFileNames: '[name].cjs', // 또는 [name].[format].js 같은 방식으로 설정 가능
     },
     {
       dir: 'dist',
       format: 'es',
-      // minifyInternalExports: true,
-      // sourcemap: true,
+      minifyInternalExports: true,
+      sourcemap: true,
       entryFileNames: '[name].mjs',
     },
   ],
